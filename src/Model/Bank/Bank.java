9855c2b0b7e108d55e33f4f86bf26ca7b799f4ab
SIddharth Dhanuka <nonsense171@gmail.com>
2022-12-10 15:28:20 -0500

    resolving conflicts

--- src/Model/Bank/Bank.java
+++ src/Model/Bank/Bank.java
@@ -1,26 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Model.Bank;
-
-import Model.Account;
-import java.util.ArrayList;
-
-/**
- *
- * @author friedahuang
- */
-//try git
-public class Bank {
-    ArrayList<Account> accounts;
-
-    public Bank(ArrayList<Account> accounts) {
-        this.accounts = accounts;
-    }
-    
-    public void transaction(Account from, Account to, long amount) {
-        from.setBalance(from.getBalance() - amount);
-        to.setBalance(to.getBalance() + amount);
-    }
-}
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package Model.Bank;
+//
+//import Model.Account;
+//import java.util.ArrayList;
+//
+///**
+// *
+// * @author friedahuang
+// */
+////try git
+//public class Bank {
+//    ArrayList<Account> accounts;
+//
+//    public Bank(ArrayList<Account> accounts) {
+//        this.accounts = accounts;
+//    }
+//    
+//    public void transaction(Account from, Account to, long amount) {
+//        from.setBalance(from.getBalance() - amount);
+//        to.setBalance(to.getBalance() + amount);
+//    }
+//}
