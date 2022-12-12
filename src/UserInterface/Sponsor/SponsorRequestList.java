f04dfaa27d84ebafdc39f5505e94ac065afa5e2a
SIddharth Dhanuka <nonsense171@gmail.com>
2022-12-11 22:06:09 -0500

    update events once sponsor and venue are confirmed

--- src/UserInterface/Sponsor/SponsorRequestList.java
+++ src/UserInterface/Sponsor/SponsorRequestList.java
@@ -60,7 +60,6 @@ public class SponsorRequestList extends javax.swing.JPanel {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         btnAccept = new javax.swing.JButton();
         btnDeny = new javax.swing.JButton();
@@ -70,13 +69,6 @@ public class SponsorRequestList extends javax.swing.JPanel {
         setBackground(new java.awt.Color(189, 212, 231));
 
         jLabel1.setText("Welcome, Sponsor");
-
-        jButton1.setText("View Organiser History");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
-            }
-        });
 
         jButton2.setText("Logout");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
@@ -135,8 +127,6 @@ public class SponsorRequestList extends javax.swing.JPanel {
                         .addComponent(btnDeny))
                     .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                         .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton1)
                         .addGap(100, 100, 100))
                     .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 524, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(343, 343, 343))
@@ -144,10 +134,8 @@ public class SponsorRequestList extends javax.swing.JPanel {
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(49, 49, 49)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jButton1))
+                .addGap(52, 52, 52)
+                .addComponent(jLabel1)
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 248, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
@@ -155,16 +143,9 @@ public class SponsorRequestList extends javax.swing.JPanel {
                     .addComponent(btnAccept)
                     .addComponent(btnDeny)
                     .addComponent(jButton2))
-                .addContainerGap(138, Short.MAX_VALUE))
+                .addContainerGap(142, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        OrganiserHistory organiserHistory = new OrganiserHistory(CardSequencePanel);
-        CardSequencePanel.removeAll();
-        CardSequencePanel.add("Organizer event history", organiserHistory);
-        ((java.awt.CardLayout) CardSequencePanel.getLayout()).next(CardSequencePanel);
-    }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
 //        SponsorEventDetails sponsorEventDetails = new SponsorEventDetails(CardSequencePanel);
@@ -230,7 +211,6 @@ public class SponsorRequestList extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAccept;
     private javax.swing.JButton btnDeny;
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
